d46fe8586bf1f6efc99a9223b5e9a5fc56a8b1a4
cGIfl300 <cgifl300@gmail.com>
2021-02-12 16:25:02 +0100

    Updating python path

--- call_internet_website.py
+++ call_internet_website.py
@@ -1,12 +1,14 @@
 # Copyright(C] 2021 cGIfl300 <cgifl300@gmail.com>
 
 from subprocess import run
+from sys import executable
 
 
 def call_internet_website():
     """ If the script doesn't work, you should review the path of python interpreter below
     :return: a json object
     """
-    shell_stdout = run("venv\\Scripts\\python.exe speedtest.py --json", shell=True, capture_output=True)
+    path_to_python = executable
+    shell_stdout = run(f"{path_to_python} speedtest.py --json", shell=True, capture_output=True)
     shell_stdout = shell_stdout.stdout.decode()
     return shell_stdout
